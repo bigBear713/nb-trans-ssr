2d648a9d85be6784f075fe5f8eb287356a47fd70
bigBear713 <774326739@qq.com>
2023-12-29 21:48:51 +0800

    update ssr config.service

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,58 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { WINDOW_TOKEN } from './window.token';
+import { NB_TRANS_DEFAULT_LANG, NB_TRANS_LOADER, NbTransLang } from '@bigbear713/nb-trans';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    importProvidersFrom(HttpClientModule),
+    {
+      provide: WINDOW_TOKEN,
+      useFactory: () => {
+        return typeof window === 'undefined' ? {} : window;
+      },
+      deps: []
+    },
+    // {
+    //   provide: NB_TRANS_MAX_RETRY,
+    //   useValue: 0
+    // },
+    {
+      provide: NB_TRANS_DEFAULT_LANG,
+      useValue: NbTransLang.ZH_CN,
+    },
+    // {
+    //   provide: NB_TRANS_LOADER,
+    //   useValue: {
+    //     // dyn load and the content is a ts file
+    //     [NbTransLang.EN]: () => import('./localization/en/translations').then(data => data.trans),
+    //     [NbTransLang.ZH_CN]: () => import('./localization/zh-CN/translations').then(data => data.trans),
+    //     // direct load
+    //     // [NbTransLang.ZH_CN]: trans,
+    //   },
+    // },
+    {
+      provide: NB_TRANS_LOADER,
+      useFactory: (http: HttpClient) => ({
+        // https://github.com/ngx-translate/core/issues/1207#issuecomment-673921899
+        // it is expecting to get the translation file using HTTP via absolute URL when angualr SSR.
+        // So here change the file's path as relative/absolute via environment
+
+        // dyn load and the content is a json file
+        // [NbTransLang.EN]: () => http.get('./assets/localization/en/translations.json').toPromise(),
+        [NbTransLang.EN]: () => http.get('http://localhost:4000/assets/localization/en/translations.json'),
+        // [NbTransLang.ZH_CN]: () => http.get('./assets/localization/zh-CN/translations.json').toPromise(),
+        [NbTransLang.ZH_CN]: () => http.get('http://localhost:4000/assets/localization/zh-CN/translations.json'),
+      }),
+      deps: [HttpClient]
+    },
+    // { provide: NB_TRANS_PARAM_KEY_INVALID_WARNING, useValue: false }
+    provideRouter(routes)
   ]
 };
 
