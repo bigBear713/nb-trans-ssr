9b8b6ba6102b47bfbac39d64d8c342c95873c33b
bigBear713 <774326739@qq.com>
2024-01-13 23:58:50 +0800

    update ssr config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    transFileDomain: 'http://localhost:4000/',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    transFileDomain: 'http://localhost:4200/',
+};
